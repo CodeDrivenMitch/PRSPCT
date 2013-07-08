6ccab7126713a4f7ab5bd7b7cd21ce0ab8c9e695
Mitchell Herrijgers <Mitchellherrijgers@gmail.com>
2013-07-08 21:33:07 +0200

    Single news item functionality

--- gen/com/insidion/prspct/R.java
+++ gen/com/insidion/prspct/R.java
@@ -36,7 +36,7 @@ public final class R {
         public static final int prspct_logo=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f090011;
+        public static final int action_settings=0x7f090013;
         public static final int bArtists=0x7f090005;
         public static final int bCreateNI=0x7f09000d;
         public static final int bEvents=0x7f090003;
@@ -45,15 +45,17 @@ public final class R {
         public static final int editText1=0x7f09000c;
         public static final int etContentNewsItem=0x7f09000b;
         public static final int etTitleNewsItem=0x7f09000a;
-        public static final int imageView1=0x7f09000e;
-        public static final int imgIcon=0x7f09000f;
+        public static final int imageView1=0x7f090010;
+        public static final int imgIcon=0x7f090011;
         public static final int info_button=0x7f090007;
         public static final int prspct_logo=0x7f090001;
         public static final int rlBg=0x7f090000;
         public static final int settings_button=0x7f090006;
         public static final int share_button=0x7f090008;
         public static final int textView1=0x7f090009;
-        public static final int txtTitle=0x7f090010;
+        public static final int tvContent=0x7f09000f;
+        public static final int tvTitle=0x7f09000e;
+        public static final int txtTitle=0x7f090012;
     }
     public static final class layout {
         public static final int activity_artists=0x7f030000;
--- src/com/insidion/prspct/SingleNewsItemActivity.java
+++ src/com/insidion/prspct/SingleNewsItemActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SingleNewsItemActivity extends Activity {
@@ -11,10 +13,18 @@ public class SingleNewsItemActivity extends Activity {
 	private NewsItemDatasource DAO;
 	private NewsItem newsItem;
 	
+	private TextView title, date, content;
+	
+	//TODO: Insert imageviews to layout
+	private ImageView icon, featured;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_single_news_item);
+		
+		title = (TextView) findViewById(R.id.tvTitle);
+		content = (TextView) findViewById(R.id.tvContent);
 		
 		DAO = new NewsItemDatasource(this);
 		DAO.open();
@@ -25,8 +35,12 @@ public class SingleNewsItemActivity extends Activity {
 		if(itemID != -1)
 		{
 			this.newsItem = DAO.getSingleNewsItem(itemID);
+			title.setText("" + this.newsItem.getTitle());
+			content.setText("" + this.newsItem.getContent());
 			
-			Toast.makeText(this, "Item id = " + itemID + " with title " + newsItem.getTitle(), Toast.LENGTH_LONG).show();
+		}
+		else {
+			Toast.makeText(this, "This newsitem was not found! Something went wrong, please try again!", Toast.LENGTH_LONG).show();
 		}
 		
 	}
